a5bdeb627c1a81d31d377a3d12634f2be7961911
windsnow1025 <windsnow1025@gmail.com>
2024-03-14 05:25:44 -0700

    fix(spring): schema mysql syntax

--- spring/src/main/resources/schema.sql
+++ spring/src/main/resources/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
 );
