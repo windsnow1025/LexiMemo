72c218df98482a19cdfd8db39af73ecd13065539
TastyPosionFly <w1300619470@hotmail.com>
2024-03-18 23:22:00 +0800

    update words, user_word table
    update words、user_word TABLE

--- spring/src/main/resources/schema.sql
+++ spring/src/main/resources/schema.sql
@@ -3,3 +3,22 @@ CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS words (
+    word_id INT AUTO_INCREMENT PRIMARY KEY ,
+    word VARCHAR(255) NOT NULL ,
+    translation VARCHAR(255) NOT NULL ,
+    example_sentence TEXT,
+    frequency INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS user_word(
+    id INT AUTO_INCREMENT PRIMARY KEY ,
+    user_id INT NOT NULL ,
+    word_id INT NOT NULL ,
+    weight DOUBLE,
+    day DATE,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (word_id) REFERENCES words(word_id)
+)
+
