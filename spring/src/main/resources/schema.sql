19ad248a0dd4ca3f6ac9c835c835543cc3597843
windsnow1025 <windsnow1025@gmail.com>
2024-03-21 08:46:50 -0700

    feat(spring): user word composite pk

--- spring/src/main/resources/schema.sql
+++ spring/src/main/resources/schema.sql
@@ -1,25 +1,28 @@
-CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS users
+(
+    id       INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     user_type VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS words (
-    word_id INT AUTO_INCREMENT PRIMARY KEY ,
-    word VARCHAR(255) NOT NULL ,
-    translation VARCHAR(255) NOT NULL ,
+CREATE TABLE IF NOT EXISTS words
+(
+    id               INT AUTO_INCREMENT PRIMARY KEY,
+    word             VARCHAR(255) NOT NULL,
+    translation      VARCHAR(255) NOT NULL,
     example_sentence TEXT,
-    frequency INT NOT NULL
+    frequency        INT          NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS user_word(
-    id INT AUTO_INCREMENT PRIMARY KEY ,
-    user_id INT NOT NULL ,
-    word_id INT NOT NULL ,
-    weight DOUBLE,
-    day DATE,
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (word_id) REFERENCES words(word_id)
+CREATE TABLE IF NOT EXISTS user_word
+(
+    user_id INT NOT NULL,
+    word_id INT NOT NULL,
+    weight  DOUBLE,
+    day     DATE,
+    PRIMARY KEY (user_id, word_id),
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (word_id) REFERENCES words (id)
 )
 
