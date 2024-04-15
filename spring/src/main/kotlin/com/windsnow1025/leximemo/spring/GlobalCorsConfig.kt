48c197692523b2d2005251cdfb1f0274bf3dd82f
windsnow1025 <windsnow1025@gmail.com>
2024-04-14 23:11:49 -0700

    feat(spring): WebMvcConfingurer

--- spring/src/main/kotlin/com/windsnow1025/leximemo/spring/GlobalCorsConfig.kt
+++ spring/src/main/kotlin/com/windsnow1025/leximemo/spring/GlobalCorsConfig.kt
@@ -1,24 +1,17 @@
 package com.windsnow1025.leximemo.spring
 
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class GlobalCorsConfig {
+class WebConfig : WebMvcConfigurer {
 
-    @Bean
-    fun corsConfigurer(): WebMvcConfigurer {
-        return object : WebMvcConfigurer {
-            override fun addCorsMappings(registry: CorsRegistry) {
-                registry.addMapping("/**")
-                    .allowedOriginPatterns("*")
-                    .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-                    .allowedHeaders("*")
-                    .allowCredentials(true)
-                    .maxAge(3600)
-            }
-        }
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("*")
+            .allowedHeaders("*")
+            .allowCredentials(true)
     }
 }
