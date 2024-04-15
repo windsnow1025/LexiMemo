dcccd35a38b10def6d0aba366cc4eb948361d264
fx1181855890 <1181855890@qq.com>
2024-04-15 10:50:32 +0800

    Update DictionaryService.js

--- next/src/service/DictionaryService.js
+++ next/src/service/DictionaryService.js
@@ -1,5 +1,24 @@
 import axios from 'axios';
 
+export default class DictionaryService{
+    constructor() {
+        this.axiosInstance = axios.create({ baseURL: process.env.NEXT_PUBLIC_NODE_API_BASE_URL });
+    }
+
+    async getDictionaries(token) {
+        try {
+            const response = await this.axiosInstance.get('/dictionaries', {
+                headers: { Authorization: token }
+            });
+            return response.data;
+        } catch (error) {
+            console.error('Failed to fetch dictionaries:', error);
+            throw new Error('Failed to fetch dictionaries.');
+        }
+    }
+
+}
+
 async function getDictionaries(token) {
     try {
         const response = await axios.get('/dictionaries', {
