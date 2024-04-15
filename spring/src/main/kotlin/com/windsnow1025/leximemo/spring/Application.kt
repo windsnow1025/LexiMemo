10ec29b7f1fceedf752675fb23ca5a99e4534a8e
windsnow1025 <windsnow1025@gmail.com>
2024-04-14 21:20:50 -0700

    feat(spring): cors

--- spring/src/main/kotlin/com/windsnow1025/leximemo/spring/Application.kt
+++ spring/src/main/kotlin/com/windsnow1025/leximemo/spring/Application.kt
@@ -2,8 +2,10 @@ package com.windsnow1025.leximemo.spring
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.ServletComponentScan
 
 @SpringBootApplication
+@ServletComponentScan
 class Application
 
 fun main(args: Array<String>) {
