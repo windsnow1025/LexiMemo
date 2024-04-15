ba6e5462581f3642198d23c24942aeb9f0172148
fx1181855890 <1181855890@qq.com>
2024-04-15 10:57:24 +0800

    Update DictionaryService.js

--- next/src/service/DictionaryService.js
+++ next/src/service/DictionaryService.js
@@ -19,14 +19,3 @@ export default class DictionaryService{
 
 }
 
-async function getDictionaries(token) {
-    try {
-        const response = await axios.get('/dictionaries', {
-            headers: { Authorization: token }
-        });
-        return response.data;
-    } catch (error) {
-        console.error('Failed to fetch dictionaries:', error);
-        throw new Error('Failed to fetch dictionaries.');
-    }
-}
