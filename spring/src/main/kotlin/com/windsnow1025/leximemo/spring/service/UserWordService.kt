b9bd44c2b0c072a1cfdae524a5f40bd32e40e249
TastyPosionFly <w1300619470@hotmail.com>
2024-05-06 10:52:02 +0800

    delete user word

--- spring/src/main/kotlin/com/windsnow1025/leximemo/spring/service/UserWordService.kt
+++ spring/src/main/kotlin/com/windsnow1025/leximemo/spring/service/UserWordService.kt
@@ -1,4 +1,33 @@
 package com.windsnow1025.leximemo.spring.service
 
-class UserWordService {
+import com.windsnow1025.leximemo.spring.logic.parseUsernameFromToken
+import com.windsnow1025.leximemo.spring.repository.UserRepository
+import com.windsnow1025.leximemo.spring.repository.UserWordRepository
+import org.springframework.dao.DataAccessException
+import org.springframework.stereotype.Service
+
+@Service
+class UserWordService(
+    private val userWordRepository: UserWordRepository,
+    private val userRepository: UserRepository
+) {
+    fun deleteUserWordByWordId(token: String, wordId: Int): Boolean {
+        val username = parseUsernameFromToken(token);
+        val userId = userRepository.findByUsername(username)?.id
+        if (userId != null) {
+            val userWord = userWordRepository.findByUserIdAndWordId(userId, wordId)
+            if (userWord != null) {
+                try {
+                    userWordRepository.delete(userWord)
+                    return true
+                } catch (e : DataAccessException){
+                    throw e
+                }
+            } else {
+                return false
+            }
+        } else {
+            return false
+        }
+    }
 }
