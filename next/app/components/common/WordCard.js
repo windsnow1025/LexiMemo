7884774873bb9ff7ea0a8589ab0e99494085dbd3
windsnow1025 <windsnow1025@gmail.com>
2024-05-27 02:40:13 +0000

    feat(next): show word memory status count

--- next/app/components/common/WordCard.js
+++ next/app/components/common/WordCard.js
@@ -29,18 +29,14 @@ const WordCard = () => {
                 const token = localStorage.getItem('token');
                 const userWords = await userLogic.getUserWord(token);
                 console.log("User words:", userWords);
-                // Sort words by day in ascending order
                 const sortedWords = userWords.sort((a, b) => new Date(a.nextDate) - new Date(b.nextDate));
-                // Limit to first 5 words
                 const limitedWords = sortedWords.slice(0, 5).map(wordObj => {
-                    // Initialize companion array for each word
-                    return { ...wordObj, companionArray: [] };
+                    return { ...wordObj, companionArray: [], stats: { known: 0, blurred: 0, notKnown: 0 } };
                 });
                 console.log("Limited words:", limitedWords);
                 setWords(limitedWords);
             } catch (error) {
                 console.error("Error fetching user words:", error);
-                // Handle error
             }
         }
         fetchWords();
@@ -51,23 +47,21 @@ const WordCard = () => {
     };
 
     const handleNextWord = () => {
-        // Check for consecutive '1's before moving to next word
         checkConsecutiveOnes(words[0].companionArray);
         if (words.length > 1) {
             setCurrentIndex(0);
-            setShowTranslation(false); // Reset translation display
-            setShowExampleSentence(false); // Reset example sentence display
-            setShowNextButton(false); // Reset next button display
+            setShowTranslation(false);
+            setShowExampleSentence(false);
+            setShowNextButton(false);
         } else {
-            // All words are finished
             setFinished(true);
         }
     };
 
     const handleKnow = () => {
-        // Update companion array for the current word
         const updatedWords = [...words];
         updatedWords[0].companionArray.push(1);
+        updatedWords[0].stats.known += 1;
         setWords(updatedWords);
         setShowTranslation(true);
         setShowExampleSentence(true);
@@ -75,9 +69,9 @@ const WordCard = () => {
     };
 
     const handleBlur = () => {
-        // Update companion array for the current word
         const updatedWords = [...words];
         updatedWords[0].companionArray.push(-1);
+        updatedWords[0].stats.blurred += 1;
         setWords(updatedWords);
         setShowTranslation(true);
         setShowExampleSentence(true);
@@ -85,9 +79,9 @@ const WordCard = () => {
     };
 
     const handleNotKnow = () => {
-        // Update companion array for the current word
         const updatedWords = [...words];
         updatedWords[0].companionArray.push(0);
+        updatedWords[0].stats.notKnown += 1;
         setWords(updatedWords);
         setShowTranslation(true);
         setShowExampleSentence(true);
@@ -95,19 +89,16 @@ const WordCard = () => {
     };
 
     const checkConsecutiveOnes = async (companionArray) => {
-        // Implement your logic to check for consecutive '1's here
         const lastTwo = companionArray.slice(-2);
         if (lastTwo[0] === 1 && lastTwo[1] === 1){
             const { zeros, ones, negatives } = countZerosOnesAndNegatives(companionArray);
             const weightString = words[0].weights;
             const weightArray = JSON.parse(weightString);
-            //把[zeros, ones, negatives]加入weightArray中
             weightArray.push([zeros, negatives, ones]);
-            const prevIntervalDays = words[0].days
+            const prevIntervalDays = words[0].days;
             const oldDate = words[0].nextDate;
             const prevIntervalDaysArray = JSON.parse(prevIntervalDays);
             const nextInterval = await getNextIntervalFromData(weightArray, prevIntervalDaysArray);
-            //newDate为oldDate加上NextInterval的天数
             const newDate = new Date(oldDate);
             newDate.setDate(newDate.getDate() + nextInterval);
             const updatedNewDate = newDate.toISOString().slice(0, 10);
@@ -123,7 +114,6 @@ const WordCard = () => {
             setWords(updatedWords);
             setCurrentIndex(0);
         } else {
-            // Insert the word at a random position in limitedWords array
             const randomIndex = Math.floor(Math.random() * (words.length - currentIndex)) + currentIndex;
             const updatedWords = [...words];
             const currentWord = updatedWords[0];
@@ -148,72 +138,74 @@ const WordCard = () => {
         return count;
     };
 
-
     const handleCloseSnackbar = () => {
         setSnackbarOpen(false);
     };
 
     return (
-        <>
-            <Card sx={{ maxWidth: 500 }}>
-                <CardContent>
-                    <Box sx={{ textAlign: 'center' }}>
-                        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                            Word of the Day
+      <>
+          <Card sx={{ maxWidth: 500 }}>
+              <CardContent>
+                  <Box sx={{ textAlign: 'center' }}>
+                      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                          Word of the Day
+                      </Typography>
+                      {finished ? (
+                        <Typography variant="h5" component="div">
+                            今日单词已背完，恭喜你！
                         </Typography>
-                        {finished ? (
-                            <Typography variant="h5" component="div">
-                                今日单词已背完，恭喜你！
-                            </Typography>
-                        ) : (
-                            <>
-                                <Typography variant="h5" component="div">
-                                    {words[0]?.word?.word}
-                                </Typography>
-                                {showTranslation && (
-                                    <Typography variant="body1" component="div">
-                                        翻译: {words[0]?.word?.translation}
-                                    </Typography>
-                                )}
-                                {showExampleSentence && (
-                                    <Typography variant="body1" component="div">
-                                        例句: {words[0]?.word?.exampleSentence}
-                                    </Typography>
-                                )}
-                            </>
-                        )}
-                    </Box>
-                </CardContent>
-                <CardActions sx={{ justifyContent: 'center' }}>
-                    {!finished && !showNextButton && (
+                      ) : (
                         <>
-                            <Button size="small" variant="contained" onClick={handleKnow}>
-                                认识
-                            </Button>
-                            <Button size="small" onClick={handleBlur}>
-                                模糊
-                            </Button>
-                            <Button size="small" variant="contained" onClick={handleNotKnow}>
-                                不认识
-                            </Button>
+                            <Typography variant="h5" component="div">
+                                {words[0]?.word?.word}
+                            </Typography>
+                            {showTranslation && (
+                              <Typography variant="body1" component="div">
+                                  翻译: {words[0]?.word?.translation}
+                              </Typography>
+                            )}
+                            {showExampleSentence && (
+                              <Typography variant="body1" component="div">
+                                  例句: {words[0]?.word?.exampleSentence}
+                              </Typography>
+                            )}
+                            <Typography variant="body2" component="div">
+                                记住: {words[0]?.stats?.known} 次 | 模糊: {words[0]?.stats?.blurred} 次 | 忘记: {words[0]?.stats?.notKnown} 次
+                            </Typography>
                         </>
-                    )}
-                    {showNextButton && (
-                        <Button size="small" variant="contained" onClick={handleNextWord}>
-                            下一个
+                      )}
+                  </Box>
+              </CardContent>
+              <CardActions sx={{ justifyContent: 'center' }}>
+                  {!finished && !showNextButton && (
+                    <>
+                        <Button size="small" variant="contained" onClick={handleKnow}>
+                            认识
                         </Button>
-                    )}
-                </CardActions>
-                <Button size="small" onClick={() => handlePlayAudio(words[0]?.word?.word)} sx={{ justifyContent: 'center' }}>
-                    <PlayArrowIcon />
-                </Button>
-            </Card>
-            <Snackbar open={snackbarOpen} autoHideDuration={6000} onClose={handleCloseSnackbar}>
-                <MuiAlert onClose={handleCloseSnackbar} severity="info" sx={{ width: '100%' }}>
-                    {snackbarMessage}
-                </MuiAlert>
-            </Snackbar>
-        </>
+                        <Button size="small" onClick={handleBlur}>
+                            模糊
+                        </Button>
+                        <Button size="small" variant="contained" onClick={handleNotKnow}>
+                            不认识
+                        </Button>
+                    </>
+                  )}
+                  {showNextButton && (
+                    <Button size="small" variant="contained" onClick={handleNextWord}>
+                        下一个
+                    </Button>
+                  )}
+              </CardActions>
+              <Button size="small" onClick={() => handlePlayAudio(words[0]?.word?.word)} sx={{ justifyContent: 'center' }}>
+                  <PlayArrowIcon />
+              </Button>
+          </Card>
+          <Snackbar open={snackbarOpen} autoHideDuration={6000} onClose={handleCloseSnackbar}>
+              <MuiAlert onClose={handleCloseSnackbar} severity="info" sx={{ width: '100%' }}>
+                  {snackbarMessage}
+              </MuiAlert>
+          </Snackbar>
+      </>
     );
 };
 
