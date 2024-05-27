b41d0e082d6bde4c83bb88e74897deeeeed434a3
Admiraljj <89696583@qq.com>
2024-05-27 11:18:29 +0800

    完成数据分析功能

--- next/app/components/common/DataChart.js
+++ next/app/components/common/DataChart.js
@@ -2,16 +2,48 @@ import * as React from 'react';
 import { LineChart } from '@mui/x-charts/LineChart';
 import CardContent from "@mui/material/CardContent";
 import Card from "@mui/material/Card";
+import { UserLogic } from '../../../src/logic/UserLogic';
 
 export default function BasicLineChart() {
+    const [data, setData] = React.useState([]);
+
+    React.useEffect(() => {
+        async function fetchData() {
+            try {
+                const userLogic = new UserLogic();
+                const token = localStorage.getItem('token');
+                const userWords = await userLogic.getUserWord(token);
+                let daysArray = [];
+
+                userWords.forEach(word => {
+                    const days = JSON.parse(word.days);
+                    if (daysArray.length === 0) {
+                        daysArray = days;
+                    } else {
+                        days.forEach((day, index) => {
+                            daysArray[index] = (daysArray[index] || 0) + day;
+                        });
+                    }
+                });
+
+                const invertedData = daysArray.map(day => 1 / day);
+                setData(invertedData);
+            } catch (error) {
+                console.error("Error fetching user words:", error);
+            }
+        }
+
+        fetchData();
+    }, []);
+
     return (
         <Card sx={{ maxWidth: 1000 }}>
             <CardContent>
                 <LineChart
-                    xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+                    xAxis={[{ data: data.map((_, index) => index + 1) }]}
                     series={[
                         {
-                            data: [2, 5.5, 2, 8.5, 1.5, 5],
+                            data: data,
                         },
                     ]}
                     width={500}
